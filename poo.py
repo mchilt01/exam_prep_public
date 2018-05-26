3cd98eebc9cd06a95bae829f8e48c5a5782c8cec
ppexam160@DCS.BBK.AC.UK <ppexam160@DCS.BBK.AC.UK>
2018-05-26 16:31:03 +0100

    func added

--- poo.py
+++ poo.py
@@ -5,9 +5,15 @@ def add_numbers(x, y):
 def sub_nums(x, y):
     return x - y
 
+def hello(x, y):
+    return x * y
+
+def goodbye(x, y):
+    return x / y
+
 x = 2
 y = 3
 print(add_numbers(x, y))
 print(sub_nums(x, y))
-
+print(hello(x, y))
 
