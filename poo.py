86546f1a2249d4cdd4b6c0cd2d6506527e68d429
ppexam160@DCS.BBK.AC.UK <ppexam160@DCS.BBK.AC.UK>
2018-05-26 16:02:52 +0100

    hello

---
+++ poo.py
@@ -0,0 +1,10 @@
+def add_numbers(x, y):
+    return x + y
+
+
+x = 2
+y = 3
+print(add_numbers(x, y))
+
+
+hello
---
+++ test_poo.py
@@ -0,0 +1,4 @@
+from poo import *
+
+def test_add_numbers():
+    assert(add_numbers(3, 4) == 7)
